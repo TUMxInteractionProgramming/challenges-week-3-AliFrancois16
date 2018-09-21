aa65678594a17d1c5b41b8ac8ea37719954ea21f
AliFrancois16 <37365993+AliFrancois16@users.noreply.github.com>
2018-09-22 00:07:43 +0200

    Challenge-6

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ $('li').on("click", function switchChannel(params) {
     console.log(chosedLi);
     $("#chat h1").html(chosedLi + "&nbsp;<small>by <strong>upgrading.never.helps</strong></small><img src='http://ip.lfe.mw.tum.de/sections/star.png' alt='starred' class='channel-meta'>");
 })
+$("#emojis-button").on("click", function() {
+    $("#emojis").toggle();
+});
