ff43f6c143c9b802f4517926dd63ed89df251b88
AliFrancois16 <37365993+AliFrancois16@users.noreply.github.com>
2018-09-22 00:32:50 +0200

    assement 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 console.log('app is alive');
-$('li').on("click", function switchChannel(params) {
+$('li').on("click", function() {
     let chosedLi = $(this).text();
     console.log('Tuning into channel ' + chosedLi);
     $("#chat h1").html(chosedLi + "&nbsp;<small>by <strong>upgrading.never.helps</strong></small><img src='http://ip.lfe.mw.tum.de/sections/star.png' alt='starred' class='channel-meta'>");
