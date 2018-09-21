2783394f5f01f99493e9e6d16abbee4cf8804f6c
AliFrancois16 <37365993+AliFrancois16@users.noreply.github.com>
2018-09-22 00:30:14 +0200

    Peer Assessment 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log('app is alive');
 $('li').on("click", function switchChannel(params) {
     let chosedLi = $(this).text();
-    console.log(chosedLi);
+    console.log('Tuning into channel ' + chosedLi);
     $("#chat h1").html(chosedLi + "&nbsp;<small>by <strong>upgrading.never.helps</strong></small><img src='http://ip.lfe.mw.tum.de/sections/star.png' alt='starred' class='channel-meta'>");
 });
 $("#emojis-button").on("click", function() {
